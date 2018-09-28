dc191d5f9e97c37d144beac5ccca7e8b3f14af20
usmanmughalji <35332218+usmanmughalji@users.noreply.github.com>
2018-09-28 18:55:18 +0500

    Update Sales Prediction.cpp

--- Sales Prediction.cpp
+++ Sales Prediction.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 
 {
-	double percentageofTotalSales = .58;
-	double salesThisYear = 8.6;
-	double result = salesThisYear * percentageofTotalSales;
+	float percentageofTotalSales = .58;
+	float salesThisYear = 8.6;
+	float result = salesThisYear * percentageofTotalSales;
 	cout<<"The Total Sales That The East Cost Divison Made This Year Was $" <<result<< " million. "<<endl;
 	getch();
 	return 0;
